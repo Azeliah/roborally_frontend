29aa4137b57edbed2e71321f4d797ac5ae3f6d8d
Andreas Carlsen <andreas.carlsen98@gmail.com>
2021-06-16 10:19:21 +0200

    Added "Add user" button next to games

--- src/components/GameComponent.tsx
+++ src/components/GameComponent.tsx
@@ -17,13 +17,19 @@ export const GameComponent: FunctionComponent<GameComponentProps> = ({game}) => 
         setBoardCreated(true)
     }
 
+    const addUserToGame = async () => {
+
+    }
+
     const [boardCreated, setBoardCreated] = useState(false)
 
     //game.users.forEach((user)=> {console.log(user)} )
     return (
         <div>
             <div>
-                <b>{game.id} : {game.name} {!boardCreated ? <button onClick={onClickGame}>Start Game</button> : ""}</b>
+                <b>{game.id} : {game.name} <button onClick={addUserToGame}>Add user</button>
+                    {!boardCreated ? <button onClick={onClickGame}>Start Game</button> : ""}
+                </b>
             </div>
 
             <ul>
